2ea0e42500c436462b67737fd2dae2eb348299cb
yangzb09 <zhibin@dashbase.io>
2019-10-15 16:09:49 +0800

    Remove alert test here

--- monitor/dashbase-prometheus/run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker run -ti --entrypoint="/bin/sh" --volume=${DIR}:/prom -w /prom prom/prometheus:v2.7.1 -c 'promtool check rules dashbase-recording/* && promtool check rules dashbase-alerts/* && promtool test rules tests/*'
