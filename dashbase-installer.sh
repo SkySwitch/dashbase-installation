550ec47237020d4d8652b63315a816583bfe7da6
John <john@Johns-MacBook-Pro.local>
2019-11-17 17:25:48 -0800

    added default values.yml

--- dashbase-installer.sh
+++ dashbase-installer.sh
@@ -3,4 +3,5 @@
 # make sure dashbase home directory exists
 mkdir -p ~/.dashbase/collector
 export DASHBASE_HOME=~/.dashbase
+curl https://raw.githubusercontent.com/dashbase/dashbase-installation/d5f450c12eb837538780142765a85848ce27635d/configs/dashcomm/values.yml > $DASHBASE_HOME/values.yml
 docker run -it -v "$DASHBASE_HOME":"/usr/local/lib/dashbase/" dashbase/dashbase-admin
