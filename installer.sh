e4aa7226d207323da72cab2a80e417883ce6b92c
Kevin <kevin@dashbase.io>
2019-11-27 17:32:16 -0800

    remove test scripts

--- installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/dashbase/dashbase-installation/admin_installer/dashbase-admin.yaml | kubectl apply -f -
-echo 'Waiting for dashbase-admin pod to be ready...'
-kubectl wait pod/dashbase-admin-0 --for condition=Ready --timeout=2m
-echo 'Pod is ready. Running init script...'
-kubectl exec -it pod/dashbase-admin-0 ./init.sh
-echo 'Done.'
