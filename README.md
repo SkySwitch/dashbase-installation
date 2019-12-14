70dbc6573f4aa3c4db23c79c9755b927c4d09a23
Ray Lui <rlui@dashbase.io>
2019-12-13 16:22:04 -0800

    DB703 simplify dashbase installation

--- README.md
+++ README.md
@@ -13,21 +13,28 @@ Give the installer permission
 chmod +x dashbase-installer.sh
 ```
 
-Run the installer, requires two arguments "submain" and optional "http"
-example below
+Run the installer, the only required input is platform flag
+example below is to create dashbase installation, with internal https, secure presto, and expose web/tables endpoints in LB.
 
 ```
-./dashbase-installer.sh raytest.dashbase.io http
+./dashbase-installer.sh --platform=aws
 ```
 
-WIP install script with input options
+install script options
 
-    --subdomain    provide subdomain field when expose via ingress
+    --platform     aws or azure or gce
+    --subdomain    provide subdomain field when expose via ingress and is required to have ingress flag be present
+                   e.g.  --subdomain=mydomain.bigdomain.io
+    --version      specify a version if not nightly will be used
+                   e.g. --version=1.0.1
     --ingress      use ingress to expose web/tables; while default is using LB
     --nopresto     not include presto in the dashbase deployment
     --nossl        nossl deployment of dashbase, this option will skip presto
+    --exposemon    expose dashbase observibility tools: pushgateway, prometheus, grafana
+    --valuefile    specify a custom dashbase value yaml file
+                   e.g. --valuefile=/tmp/mydashbase_values.yaml
     
     
-example of dashbase-installer-2.sh
+examples of using ingress, nopresto, nossl on AWS platform
 
-    ./dashbase-installer-2.sh --subdomain=raytest.dashbase.io --ingress --nopresto --nossl --platform=aws
+    ./dashbase-installer.sh --subdomain=raytest.dashbase.io --ingress --nopresto --nossl --platform=aws
---
+++ dashbase-admin/dashbase_setup_tarball/https_presto.sh
@@ -0,0 +1,121 @@
+# This script will nstall secure presto in k8s  and also will setup redash.
+# run the script example
+# ./https_presto.sh <namespace>
+
+#!/bin/bash
+# enter namespace value, not entering namespace value will auto exit
+if [ -z "$1" ]
+then
+   echo "no namespace entered"
+   echo "will use default namespace = dashbase"
+   NAMESPACE="dashbase"
+else
+   echo "entered namespace value = $1"
+   NAMESPACE=$1
+fi
+
+# remove previous presto cert, key and keystore in the current folder
+[ -e presto-keystore ] && rm -rf presto-keystore
+[ -e presto-keystore.p12 ] && rm -rf presto-keystore.p12
+[ -e presto-cert.pem ] && rm -rf presto-cert.pem
+[ -e presto-key.pem ] && rm -rf presto-key.pem
+[ -e presto_keystore_password ] && rm -rf presto_keystore_password
+[ -e https.yaml ] && rm -rf https.yaml
+
+# bash generate random 32 character alphanumeric string (upper and lowercase) and
+
+export LC_CTYPE=C
+
+KEYSTORE_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+#NAMESPACE=$1
+#echo "entered namespace is $NAMESPACE"
+echo $KEYSTORE_PASS > presto_keystore_password
+KEYSTORE_PASSWORD=$(cat presto_keystore_password)
+echo "Creating presto-keystore file"
+
+keytool -genkey -noprompt \
+ -alias dashbase \
+ -dname "CN=presto-coordinator-0.presto-coordinator.$NAMESPACE.svc.cluster.local, OU=Engineering, O=Dashbase, L=Santa clara, S=CA, C=US" \
+ -keystore presto-keystore \
+ -storepass $KEYSTORE_PASSWORD \
+ -keypass $KEYSTORE_PASSWORD \
+ -keyalg RSA  \
+ -validity 3650 \
+ -keysize 2048
+
+echo "Convert presto-keystore into p12 format and output is presto-keystore.p12 file"
+keytool -importkeystore -srckeystore presto-keystore \
+  -destkeystore presto-keystore.p12 -deststoretype PKCS12 \
+  -deststorepass $KEYSTORE_PASSWORD -srcstorepass $KEYSTORE_PASSWORD
+
+
+echo "using openssl command creating presto-cert.pem  and  presto-key.pem  file from presto-keystore.p12"
+openssl pkcs12 -in presto-keystore.p12 -nokeys -out presto-cert.pem -passin pass:$KEYSTORE_PASSWORD
+openssl pkcs12 -in presto-keystore.p12 -nodes -nocerts -out presto-key.pem -passin pass:$KEYSTORE_PASSWORD
+
+echo "signed signed-cert generation for presto is completed"
+echo "you should have the following files:"
+echo "1. presto-kestore  java keystore for presto"
+echo "2. presto-keystore.p12 P12 format file for presto-keystore"
+echo "3. presto-cert.pem base 64 cert file for presto"
+echo "4. presto-key.pem  base 64 key file for presto"
+echo "The CN of this self-signed cert is presto-coordinator-0.presto-coordinator.$NAMESPACE.svc.cluster.local"
+
+# create Base 64 encryption for generated key, cert, keystore, keystore password
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+   echo "create presto Base 64 encryption for generated key, cert, keystore, keystore password from mac workstation"
+   PRESTO_KEYSTORE_PASS_B64=`echo -n "$KEYSTORE_PASSWORD" |base64`
+   PRESTO_KEYSTORE_B64=`cat presto-keystore |base64`
+   PRESTO_CERT_B64=`cat presto-cert.pem |base64`
+   PRESTO_KEY_B64=`cat presto-key.pem |base64`
+elif [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-musl" ]]; then
+   echo "create presto Base 64 encryption for generated key, cert, keystore, keystore password from linux workstation"
+   PRESTO_KEYSTORE_PASS_B64=`echo -n "$KEYSTORE_PASSWORD" |base64 -w 0`
+   PRESTO_KEYSTORE_B64=`cat presto-keystore |base64 -w 0`
+   PRESTO_CERT_B64=`cat presto-cert.pem |base64 -w 0`
+   PRESTO_KEY_B64=`cat presto-key.pem |base64 -w 0`
+else
+   echo "OSTYPE is not supported"
+   exit
+fi
+
+#echo "Presto keystore password"
+#echo $PRESTO_KEYSTORE_PASS_B64
+#echo "####################################################"
+#echo "presto keystore"
+#echo $PRESTO_KEYSTORE_B64
+#echo "####################################################"
+#echo "presto cert"
+#echo $PRESTO_CERT_B64
+#echo "####################################################"
+#echo "presto key"
+#echo $PRESTO_KEY_B64
+#echo "####################################################"
+
+# feed the base64 outputs of key, cert, keystore, and keystore password into https-presto.yaml file
+
+echo "feed the base64 outputs of key, cert, keystore, and keystore password into https-presto.yaml file"
+cp https-presto-template.yaml https-presto.yaml
+
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+   sed -i .bak "s|KEYSTORE|${PRESTO_KEYSTORE_B64}|" https-presto.yaml
+   sed -i .bak "s|KEYPASS|${PRESTO_KEYSTORE_PASS_B64}|" https-presto.yaml
+   sed -i .bak "s|CERTPEM|${PRESTO_CERT_B64}|" https-presto.yaml
+   sed -i .bak "s|KEYPEM|${PRESTO_KEY_B64}|" https-presto.yaml
+elif [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-musl" ]]; then
+   sed -i "s|KEYSTORE|${PRESTO_KEYSTORE_B64}|" https-presto.yaml
+   sed -i "s|KEYPASS|${PRESTO_KEYSTORE_PASS_B64}|" https-presto.yaml
+   sed -i "s|CERTPEM|${PRESTO_CERT_B64}|" https-presto.yaml
+   sed -i "s|KEYPEM|${PRESTO_KEY_B64}|" https-presto.yaml
+else
+   echo "OSTYPE is not supported"
+   exit
+fi
+
+echo "https.yaml file is updated"
+echo "kubectl apply -f https.yaml -n $NAMESPACE"
+#kubectl apply -f https-presto.yaml -n $NAMESPACE
+kubectl get secrets -n $NAMESPACE |grep presto
+echo "install steps for presto SSL cert, pem, keystore on K8s cluster is completed"
