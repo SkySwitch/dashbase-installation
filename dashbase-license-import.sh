731b15dd06e4ec789f022a047a84f8e61d9edfed
yangzb09 <zhibin@dashbase.io>
2019-12-21 02:35:19 +0800

    Add --username --license input

--- dashbase-license-import.sh
+++ dashbase-license-import.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+USERNAME="undefined"
+LICENSE="undefined"
+
 function log_info() {
   echo -e "INFO *** $*"
 }
@@ -13,28 +16,73 @@ function log_fatal() {
   exit 1
 }
 
-# Check that the dashbase-license.txt is valid
-grep -e "^username: \".*\""  dashbase-license.txt > /dev/null  &&  grep -e "^license: \".*\"" dashbase-license.txt > /dev/null
-if [ $? -ne 0 ]; then
-  log_fatal "Username or license format is invaild, please check it ! "
-else
-  log_info "dashbase-license.txt is valid, passed."
-fi
+function fail_if_empty() {
+  [[ -z "$2" ]] && log_fatal "Parameter $1 must have a value."
+  return 0
+}
 
-#Update dashbase license information
+while [[ $# -gt 0 ]]; do
+  PARAM=${1%%=*}
+  [[ "$1" == *"="* ]] && VALUE=${1#*=} || VALUE=""
+  log_info "Parsing ($1)"
+  shift 1
+
+  case $PARAM in
+  --username)
+    fail_if_empty "$PARAM" "$VALUE"
+    USERNAME=$VALUE
+    ;;
+  --license)
+    fail_if_empty "$PARAM" "$VALUE"
+    LICENSE=$VALUE
+    ;;
+  *)
+    log_warning "Unknown parameter ($PARAM) with ${VALUE:-no value}"
+    ;;
+  esac
+done
+
+check_license() {
+  if [[ -z "$USERNAME" || -z "$LICENSE" ]]; then
+    log_fatal "either username or license string is missing"
+  elif [[ "$USERNAME" == "undefined" && "$LICENSE" == "undefined" ]]; then
+    log_warning "No License information is entered, install without license"
+  elif [[ "$USERNAME" != "undefined" && "$LICENSE" != "undefined" ]]; then
+    log_info "Entered username is $USERNAME"
+    log_info "Entered license string is $LICENSE"
+  else
+     log_fatal "Please check entered username and license string"
+  fi
+}
+
+# Create dashbase-license.txt
+  if [[ "$USERNAME" == "undefined" && "$LICENSE" == "undefined" ]]; then
+    log_warning "No License information is entered, install without license, no change on default dashbase-values.yaml file"
+  else
+    log_info "update default dashbase-values.yaml file with entered license information"
+    echo "username: \"$USERNAME\"" > dashbase-license.txt
+    echo "license: \"$LICENSE\"" >> dashbase-license.txt
+  fi
+
+# Update dashbase license information
 log_info "update default dashbase-values.yaml file with entered license information"
 kubectl cp dashbase-license.txt dashbase/admindash-0:/dashbase/
 kubectl exec -it admindash-0 -n dashbase -- bash -c "sed '/^username:/d;/^license:/d' /data/dashbase_values.yaml > /data/dashbase_values.yaml"
 kubectl exec -it admindash-0 -n dashbase -- bash -c "cat dashbase-license.txt >> dashbase-values.yaml"
 
-
+# Update dashbase license information
 log_info "helm upgrade dashbase chartmuseum/dashbase -f /data/dashbase_values.yaml --namespace dashbase --version $installed_version"
 kubectl exec -it admindash-0 -n dashbase -- bash -c "helm upgrade dashbase dashbase/dashbase -f /data/dashbase_values.yaml --namespace dashbase --version $(helm ls '^dashbase$' |grep 'dashbase' |  awk '{print $10}')"
 
 log_info "kubectl delete pod $(kubectl get pod -n dashbase | grep api | awk '{print $1}') -n dashbase"
 kubectl delete pod $(kubectl get pod -n dashbase | grep api | awk '{print $1}') -n dashbase
+
 log_info "kubectl wait --for=condition=Ready pod/$(kubectl get pod -n dashbase | grep api | awk '{print $1}') -n dashbase"
 kubectl wait --timeout=-1s --for=condition=available deployment/api -n dashbase
+  if [[ $? = 0 ]]; then
+    log_info "Update successful, enjoy your dashbase."
+    rm -rf ./dashbase-license.txt
+  fi
 
 
 
