b334ad28764ae87355f45b944eb9fc9fb4b20a59
Pushkar <55374704+pushkar-dashbase@users.noreply.github.com>
2019-12-21 10:09:38 +0530

    Added unit tests (#86)

--- log-collection/parsing-tools/tests/test_asterisk.py
+++ log-collection/parsing-tools/tests/test_asterisk.py
@@ -1,5 +1,7 @@
 import unittest
-from asterisk.asterisk import convert_dateformat
+from asterisk.asterisk import convert_dateformat, get_config
+from asterisk.asterisk import get_pattern
+
 
 class AsteriskTest(unittest.TestCase):
     def testBasic(self):
@@ -23,3 +25,25 @@ class AsteriskTest(unittest.TestCase):
     def testConvert3(self):
         dashbase_pattern = convert_dateformat("%F %T '\"[")
         self.assertEqual("yyyy-MM-dd HH:mm:ss '''\"['", dashbase_pattern)
+
+    def testConvert4(self):
+        dashbase_pattern = convert_dateformat("%F %T.%3q '\"[")
+        self.assertEqual("yyyy-MM-dd HH:mm:ss.SSS '''\"['", dashbase_pattern)
+
+    def testConvert5(self):
+        dashbase_pattern = convert_dateformat("%a, %d %b %y %T %z '\"[")
+        self.assertEqual("EEE, dd MMM yy HH:mm:ss Z '''\"['", dashbase_pattern)
+
+    def testConvert6(self):
+        dashbase_pattern = convert_dateformat("%D %R '\"[")
+        self.assertEqual("MM/dd/yy HH:mm '''\"['", dashbase_pattern)
+
+    def testConvert7(self):
+        dashbase_pattern = convert_dateformat("%c '\"[")
+        self.assertEqual("EEE MMM d HH:mm:ss yyyy '''\"['", dashbase_pattern)
+
+    def testGetPattern(self):
+        conf_file = "./tests/logger.conf"
+        config = get_config(conf_file)
+        dashbase_pattern = get_pattern(config)
+        self.assertEqual('\[%{GREEDYDATA:timestamp:datetime:yyyy-MM-dd HH:mm:ss}\] %{WORD:level:meta}\[%{INT:lwp:int}\] %{JAVAFILE:source:meta}: %{GREEDYDATA:message}',dashbase_pattern)
