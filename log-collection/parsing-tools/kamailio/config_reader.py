d9043a9840454de5cec2c81420a51e3c2109834f
pushkar-dashbase <pushkar@dashbase.io>
2019-12-26 15:45:11 +0530

    Fixed ArgumentParser and code cleanup

--- log-collection/parsing-tools/kamailio/config_reader.py
+++ log-collection/parsing-tools/kamailio/config_reader.py
@@ -1,44 +1,18 @@
-import argparse
 import re
-import os
 import logging
 
 
 class ConfigReader:
 
-    def __init__(self):
+    def __init__(self,conf_file, path_to_sample_logs):
         self.xLogs = []
         self.log_sample = ''
-        self.parser = argparse.ArgumentParser(
-            description='script to generate filebeat configuration file based on logging configuration file for kamailio server',
-            usage='use "python %(prog)s --help" for more information',
-            formatter_class=argparse.RawTextHelpFormatter)
-        self.log_sample_file_name = self.read_log_sample_path()
-        self.kamailio_config = self.read_kamailio_config_path()
+        self.log_sample_file_name = path_to_sample_logs
+        self.kamailio_config = conf_file
         self.read_log_sample()
         self.read_xlog_from_kamailio_config()
 
-    def read_kamailio_config_path(self):
-        self.parser.add_argument('-c', '--conf_file', help='''file to read logging configuration from.
-            Example: python kamailio.py -c /tmp/kamailio.cfg''')
-        self.args = self.parser.parse_args()
-        conf_file = self.args.conf_file
-        if not conf_file:
-            conf_file = raw_input("Enter kamailio config file (kamailio.cfg) path:")
-        return conf_file
 
-
-    def read_log_sample_path(self):
-        self.parser.add_argument('-l', '--path_to_sample_logs', help='''path to the kamailio sample logs to find date format, level, format etc.
-            Example: python kamilio.py -l /var/log/kamilio/message*''')
-        self.args = self.parser.parse_args()
-        path_to_sample_logs = self.args.path_to_sample_logs
-        if not path_to_sample_logs:
-            path_to_sample_logs = raw_input(
-                "Enter path to the kamailio sample logs to collect from [default: /var/log/kamailio/full*]:")
-            if not path_to_sample_logs:
-                path_to_logs = '../tests/log'
-        return path_to_sample_logs
 
     def read_log_sample(self):
         with open(self.log_sample_file_name) as fp:
--- log-collection/parsing-tools/kamailio/kamailio.py
+++ log-collection/parsing-tools/kamailio/kamailio.py
@@ -14,8 +14,51 @@ import logging
 dict_list = []
 
 
-def writeConfig(filebeat_file):
+def write_filebeat_config(pattern, filebeat_config, path_to_logs):
+    ''' Writes filebeat configuration for asterisk using pattern and path_to_logs
+    '''
 
+    with open(filebeat_config, "w") as fp:
+        try:
+            path = """
+- paths:
+    - """
+            document = """
+  type: log
+  multiline.pattern: '^.'
+  multiline.negate: true
+  multiline.match: after
+  close_inactive: 90s
+  harvester_limit: 1000
+  scan_frequency: 1s
+  symlinks: true
+  clean_removed: true
+  fields:
+    _message_parser:
+      type: multi
+      applyAll: 'true'
+      parsers:
+        sip:
+          type: sip
+        grok:
+          type: multi
+          parsers:
+            freepbx:
+              type: grok
+              pattern: "\\[%{TIMESTAMP_ISO8601:timestamp:datetime:yyyy-MM-dd HH:mm:ss}\\] %{WORD:level:meta}\\[%{INT:lwp:int}\\](\\[%{DATA:callid:text}\\])? %{JAVAFILE:source:meta}: %{GREEDYDATA:message}"
+            asterisk:
+              type: grok
+              pattern: """
+
+            path += '"{}"'.format(path_to_logs)
+            document += '"{}"'.format(pattern)
+            fp.write(path + document)
+            print("Created asterisk filebeat configuration file {} successfully".format(filebeat_config))
+        except Exception, e:
+            print("ERROR: Failed to output asterisk filebeat configuration file", e)
+
+
+def writeConfig(filebeat_file):
     with open(filebeat_file, "w") as fp:
         try:
             fp.truncate(0)
@@ -39,17 +82,46 @@ def main():
         usage='use "python %(prog)s --help" for more information',
         formatter_class=argparse.RawTextHelpFormatter)
 
+    parser.add_argument('-c', '--conf_file', help='''file to read logging configuration from.
+               Example: python kamailio.py -c /tmp/kamailio.cfg''')
+
     parser.add_argument('-o', '--output_filebeat_file', help='''yaml file to write created filebeat configuration.
         Example: python kamailio.py -o /tmp/kamailio.yaml''')
+
+    parser.add_argument('-l', '--path_to_sample_logs', help='''path to the kamailio sample logs to find date format, level, format etc.
+                Example: python kamailio.py -l /var/log/kamilio/message*''')
+
+    parser.add_argument('-p', '--path_to_logs', help='''path to the kamailio logs to collect from with filebeat.
+        Example: python kamailio.py -p /var/log/kamailio/message*''')
+
     args = parser.parse_args()
+    print (args)
 
     filebeat_file = args.output_filebeat_file
     if not filebeat_file:
-        tm = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H:%M:%S")
+        tm = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H:%M:%S.%f")[:-3]
         filebeat_file = os.path.join(os.getcwd(), 'kamailio-{}.yaml'.format(tm))
 
+    conf_file = args.conf_file
+    if not conf_file:
+        conf_file = raw_input("Enter kamailio config file (kamailio.cfg) path:")
+
+    path_to_sample_logs = args.path_to_sample_logs
+    if not path_to_sample_logs:
+        path_to_sample_logs = raw_input(
+            "Enter path to the kamailio sample logs to collect from [default: /var/log/kamailio/full*]:")
+        if not path_to_sample_logs:
+            path_to_logs = '../tests/log'
+
+    path_to_logs = args.path_to_logs
+    if not path_to_logs:
+        path_to_logs = raw_input(
+            "Enter path to the kamailio logs to collect from [default: /var/log/kamailio/message*]:")
+        if not path_to_logs:
+            path_to_logs = '/var/log/kamailio/message*'
+
     logging.basicConfig(level=logging.DEBUG)
-    configReader = ConfigReader()
+    configReader = ConfigReader(conf_file, path_to_sample_logs)
     builder = LogToPatternBuilder(configReader.log_sample)
     count = 0
     for xLog in configReader.xLogs:
@@ -59,8 +131,13 @@ def main():
             pattern += decoder.log_config
             logging.debug(pattern)
             count = count + 1
-            buildConfig(pattern, "patternType" + str(count))
-    writeConfig(filebeat_file)
+            time.sleep(1)
+            #buildConfig(pattern, "patternType" + str(count))
+            tm = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H:%M:%S.%f")[:-3]
+            filebeat_file = os.path.join(os.getcwd(), 'kamailio-{}.yaml'.format(tm))
+            write_filebeat_config(pattern, filebeat_file, path_to_logs)
+
+    #writeConfig(filebeat_file)
     logging.debug('Done! I am going home. (Good)Bye!')
 
 
