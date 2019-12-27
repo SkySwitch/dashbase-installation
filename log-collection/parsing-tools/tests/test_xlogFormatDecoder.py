e09203de8f5c3b43e21745b52246da582571e936
pushkar-dashbase <pushkar@dashbase.io>
2019-12-27 21:11:16 +0530

    fixed broken unittests

--- log-collection/parsing-tools/tests/test_xlogFormatDecoder.py
+++ log-collection/parsing-tools/tests/test_xlogFormatDecoder.py
@@ -8,9 +8,9 @@ class TestXlogFormatDecoder(TestCase):
     def test_get_pattern1(self):
         log = "xlog(\"L_INFO\",\"[$cfg(route)] *New request $rm* rU=$rU/tU=$tU/fU=$fU/rd=$rd/si=$si/sp=$sp \\n\");"
         decoder = XlogFormatDecoder(log)
-        self.assertEqual("[%{DATA:cfg-route}] *New request %{DATA:rm}* rU=%{DATA:rU}/tU=%{DATA:tU}/fU=%{DATA:fU}/rd=%{DATA:rd}/si=%{DATA:si}/sp=%{DATA:sp}",decoder.log_config)
+        self.assertEqual("[%{DATA:cfg-route}] *New request %{DATA:rm}* rU=%{DATA:rU}/tU=%{DATA:tU}/fU=%{DATA:fU}/rd=%{DATA:rd}/si=%{DATA:si}/sp=%{GREEDYDATA:sp}",decoder.log_config)
 
     def test_get_pattern2(self):
         log = "xlog(\"L_ALERT\",\"ALERT: pike blocking $rm from $fu (IP:$si:$sp)\\n\");"
         decoder = XlogFormatDecoder(log)
-        self.assertEqual("ALERT: pike blocking %{DATA:rm} from %{DATA:fu} (IP:%{DATA:si}:%{DATA:sp})",decoder.log_config)
+        self.assertEqual("ALERT: pike blocking %{DATA:rm} from %{DATA:fu} (IP:%{DATA:si}:%{GREEDYDATA:sp})",decoder.log_config)
