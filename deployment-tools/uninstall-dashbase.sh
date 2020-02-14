97a1824d0280e5d390189bc397abc69ddad1cdba
rluidash <56131833+rluidash@users.noreply.github.com>
2020-02-13 19:09:34 -0800

    DB-1344 new uninstall dashbase script (#121)

--- deployment-tools/uninstall-dashbase.sh
+++ deployment-tools/uninstall-dashbase.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
-#
+
+CMDS="curl kubectl"
+
 headline(){
   echo -e "###########################################################"
   echo -e "#####  Undo Dashbase Installation                     #####"
+  echo -e "#####  this script requires kubectl commands          #####"
   echo -e "###########################################################"
+}
+
+# log functions and input flag setup
+function log_info() {
+  echo -e "INFO *** $*"
+}
+
+function log_warning() {
+  echo -e "WARN *** $*"
+}
+
+function log_fatal() {
+  echo -e "FATAL *** $*"
+  exit 1
+}
+
+check_commands() {
+  for x in $CMDS
+     do command -v "$x" > /dev/null && continue || { log_fatal "$x command not found."; }
+  done
 }
 
 remove_storageclass() {
     # delete storageclass
     if [ "$(kubectl get storageclass |grep -c dashbase)" -gt "0"  ]; then
-       echo "dashbase storageclass exists"
+       log_info "dashbase storageclass exists"
        for STORECLASS in $(kubectl get storageclass |grep dashbase |awk '{print $1}' |tr "\n" " "); do
-           echo "delete storageclass $STORECLASS"
+           log_info "delete storageclass $STORECLASS"
            kubectl delete storageclass "$STORECLASS"
        done
     else
-       echo "no dashbase storageclass is found"
+       log_info "no dashbase storageclass is found"
     fi
 }
 
@@ -34,94 +57,190 @@ remove_clusterrolebindings() {
     # delete clusterrolebindings
     for CRBIND in admin-user-binding tiller ; do
        if [ "$(kubectl get clusterrolebindings |grep -c $CRBIND)" -gt "0" ]; then
-          echo "cluster role binding $CRBIND exists"
-          echo "delete cluster role  binding $CRBIND"
+          log_info "cluster role binding $CRBIND exists"
+          log_info "delete cluster role  binding $CRBIND"
           kubectl delete clusterrolebinding $CRBIND
        else
-          echo "cluster role binding $CRBIND is not found"
+          log_info "cluster role binding $CRBIND is not found"
        fi
     done
 }
 
 remove_sa_tiller() {
       if [ "$(kubectl get sa -n kube-system |grep -c tiller)" -gt "0" ]; then
-       echo "service account tiller exists"
-       echo "delete service account tiller"
+       log_info "service account tiller exists"
+       log_info "delete service account tiller"
        kubectl delete sa tiller -n kube-system
     else
-       echo "service account tiller is not found in kube-system namespace"
+       log_info "service account tiller is not found in kube-system namespace"
     fi
+}
+
+remove_ingress_via_admindash() {
+  # check if nginx is deployed , if yes delete it; and timeout after 8 minutes
+  while [ "$(kubectl exec -it admindash-0 -n dashbase -- helm ls |grep dashbase |grep -c nginx)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
+    log_info "nginx-ingress controller is dedected"
+    log_info "removing ngnix-ingress controller"
+    kubectl exec -it admindash-0 -n dashbase -- helm delete --purge nginx-ingress
+  done
+}
+
+remove_dashbase_via_admindash() {
+  # check if dashbase is deployed, if yes delete it; and timeout after 8 minutes
+  while [ "$(kubectl exec -it admindash-0 -n dashbase -- helm ls |grep dashbase |grep -c -iv nginx)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
+    log_info "dashbase components deployed via helm"
+    # remove dashbase
+    log_info "removing dashbase components via helm in admindash pod"
+    kubectl exec -it admindash-0 -n dashbase -- helm delete --purge dashbase
+  done
+}
+
+remove_ingress_via_helm() {
+  # check if nginx is deployed , if yes delete it; and timeout after 8 minutes
+  while [ "$(helm ls |grep dashbase |grep -c nginx)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
+    log_info "nginx-ingress controller is dedected"
+    log_info "removing ngnix-ingress controller"
+    helm delete --purge nginx-ingress
+  done
+}
+
+remove_dashbase_via_helm() {
+  # check if dashbase is deployed, if yes delete it; and timeout after 8 minutes
+  while [ "$(helm ls |grep dashbase |grep -c -iv nginx)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
+    log_info "dashbase components deployed via helm"
+    # remove dashbase
+    log_info "removing dashbase components via helm in admindash pod"
+    helm delete --purge dashbase
+  done
+}
+
+delete_dashbase_pvc() {
+  # delete dashbase persistent volume claim
+  if [ "$(kubectl get  pvc -n dashbase |grep -c -iv CAPACITY)" -gt "0" ]; then
+    log_info "dashbase PVC exists"
+    for PVC in $(kubectl get pvc -n dashbase |tail -n +2 |awk '{print $1}' |tr "\n" " "); do
+        log_info "delete pvc $PVC"
+        kubectl delete pvc  "$PVC" -n dashbase
+    done
+  else
+       log_info "no dashbase PVC is found in dashbase namespace"
+  fi
+}
+
+delete_dashbase_secrets() {
+  # delete dashbase secrets
+  if [ "$(kubectl get secrets -n dashbase |grep -E -c 'dashbase-key|dashbase-cert')" -gt "0" ]; then
+    log_info "dashbase secrets exists"
+    for DSECRETS in $(kubectl get secrets -n dashbase |grep -E 'dashbase-cert|dashbase-key' |awk '{print $1}' |tr "\n" " "); do
+      log_info "delete secrets $DSECRETS"
+      kubectl delete secrets "$DSECRETS" -n dashbase
+    done
+  else
+    log_info "no dashbase secrets is found in dashbase namespace"
+  fi
+  # delete presto secrets
+  if [ "$(kubectl get secrets -n dashbase |grep -E -c 'presto-key|presto-cert')" -gt "0" ]; then
+    log_info "presto secrets exists"
+    for PSECRETS in $(kubectl get secrets -n dashbase |grep -E 'presto-cert|presto-key' |awk '{print $1}' |tr "\n" " "); do
+      log_info "delete secrets $PSECRETS"
+      kubectl delete secrets "$PSECRETS" -n dashbase
+    done
+  else
+    log_info "no presto secrets is found in dashbase namespace"
+  fi
+}
+
+remove_sa_dashadmin() {
+  # delete sa dashadmin account
+  if [ "$(kubectl get sa -n dashbase |grep -c dashadmin)" -gt "0" ]; then
+    log_info "service account dashadmin exists"
+     "delete service account dashadmin"
+    kubectl delete sa dashadmin -n dashbase
+  else
+    log_info "service account dashadmin is not found in dashbase namespace"
+  fi
 }
 
 # main process below this line
 headline
 
+# check dashbase namespace has or not
 if [ "$(kubectl get namespace |grep -c dashbase)" -eq "1" ]; then
-  echo "dashbase namespace exists"
-  if [ "$(kubectl get po -n dashbase |grep -c admindash)" -eq "1" ]; then
-     echo "admin pod exists"
-     # check helm tiller
-     if [ "$(kubectl get po -n kube-system |grep -c tiller-deploy)" -gt "0" ]; then
-        echo "helm tiller is deployed in kube-system namespace"
+  log_info "dashbase namespace exists"
 
-        # check if nginx is deployed , if yes delete it; and timeout after 8 minutes
-        while [ "$(kubectl exec -it admindash-0 -n dashbase -- helm ls |grep dashbase |grep -c nginx)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
-          echo "nginx-ingress controller is dedected"
-          echo "removing ngnix-ingress controller"
-          kubectl exec -it admindash-0 -n dashbase -- helm delete --purge nginx-ingress
-        done
-        # check if dashbase is deployed, if yes delete it; and timeout after 8 minutes
-        while [ "$(kubectl exec -it admindash-0 -n dashbase -- helm ls |grep dashbase |grep -c -iv nginx)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
-          echo "dashbase components deployed"
-          # remove dashbase
-          echo "removing dashbase components"
-          kubectl exec -it admindash-0 -n dashbase -- helm delete --purge dashbase
-        done
-        # check if presto is deployed if yes delete it; and timeout after 8 minutes
-        # while [ "$(kubectl exec -it admindash-0 -n dashbase -- helm ls |grep -c presto)" -eq "1" ] && [ $SECONDS -lt 480 ]; do
-        #  echo "presto components deployed"
-        #  echo "removing presto components"
-        #  kubectl exec -it admindash-0 -n dashbase -- helm delete --purge presto
-        # done
+  # check helm tiller deploy or not
+  if [ "$(kubectl get po -n kube-system |grep -c tiller-deploy)" -gt "0" ]; then
+     log_info "helm tiller is deployed in kube-system namespace"
+
+     # check has admindash pod or not
+     if [ "$(kubectl get po -n dashbase |grep -c admindash)" -eq "1" ]; then
+        log_info "admin pod exists"
+        log_info "checking and removing any resources in dashbase namespace from admindash helm"
+        # check ingress and dashbase resources
+        remove_ingress_via_admindash
+        remove_dashbase_via_admindash
+        sleep 10
+        # delete admin pod
+        log_info "delete admin pod dashadmin-0"
+        kubectl delete sts/admindash -n dashbase
+        # delete pvc
+        delete_dashbase_pvc
+        # delete secrets
+        delete_dashbase_secrets
+        # delete storageclass
+        remove_storageclass
+        #  delete helm tiller
+        remove_tiller
+        # delete clusterrolebindings
+        remove_clusterrolebindings
+        # delete sa dashadmin account
+        remove_sa_dashadmin
+        # delete sa tiller account
+        remove_sa_tiller
+        # delete namespace
+        kubectl delete namespace dashbase
+
+     # check if helm command exists in local workstation
+     elif [ "$(command -v helm)" ] && [ "$(helm ls |grep -E -- 'dashbase|ingress')" ]; then
+        log_info "helm command exists"
+        log_info "checking and removing any resources in dashbase namespace from helm configured locally"
+        remove_ingress_via_helm
+        remove_dashbase_via_helm
+        sleep 10
+        # delete pvc
+        delete_dashbase_pvc
+        # delete secrets
+        delete_dashbase_secrets
+        # delete storageclass
+        remove_storageclass
+        #  delete helm tiller
+        remove_tiller
+        # delete clusterrolebindings
+        remove_clusterrolebindings
+        # delete sa dashadmin account
+        remove_sa_dashadmin
+        # delete sa tiller account
+        remove_sa_tiller
+        # delete namespace
+        kubectl delete namespace dashbase
      else
-        echo "helm tiller is not found in K8s cluster"
-     fi
-     
-     # delete admin pod
-     echo "delete admin pod dashadmin-0"
-     kubectl delete sts/admindash -n dashbase
-
-    # delete dashbase persistent volume claim
-    if [ "$(kubectl get  pvc -n dashbase |grep -c -iv CAPACITY)" -gt "0" ]; then
-       echo "dashbase PVC exists"
-       for PVC in $(kubectl get pvc -n dashbase |tail -n +2 |awk '{print $1}' |tr "\n" " "); do
-           echo "delete pvc $PVC"
-           kubectl delete pvc  "$PVC" -n dashbase
-       done
-    else
-       echo "no dashbase PVC is found in dashbase namespace"
+        log_fatal "helm tiller is deployed but either no admindash pod or local helm command to check any resource deployed via helm"
     fi
-    # delete dashbase secrets
-    if [ "$(kubectl get secrets -n dashbase |grep -E -c 'dashbase-key|dashbase-cert')" -gt "0" ]; then
-       echo "dashbase secrets exists"
-       for DSECRETS in $(kubectl get secrets -n dashbase |grep -E 'dashbase-cert|dashbase-key' |awk '{print $1}' |tr "\n" " "); do
-           echo "delete secrets $DSECRETS"
-           kubectl delete secrets "$DSECRETS" -n dashbase
-       done
-    else
-       echo "no dashbase secrets is found in dashbase namespace"
+  else
+    log_info "No helm tiller deployed to this K8s cluster"
+    log_info "Since no helm tiller, that means no dashbase resource deployed"
+     # check if amdindash pod exists but no helm tiller deployed
+     # this condition could be arise from a failed previous dashbase installation
+    if [ "$(kubectl get po -n dashbase |grep -c admindash)" -eq "1" ]; then
+         # delete admin pod
+        log_info "delete admin pod dashadmin-0"
+        kubectl delete sts/admindash -n dashbase
     fi
-    # delete presto secrets
-    if [ "$(kubectl get secrets -n dashbase |grep -E -c 'presto-key|presto-cert')" -gt "0" ]; then
-       echo "presto secrets exists"
-       for PSECRETS in $(kubectl get secrets -n dashbase |grep -E 'presto-cert|presto-key' |awk '{print $1}' |tr "\n" " "); do
-           echo "delete secrets $PSECRETS"
-           kubectl delete secrets "$PSECRETS" -n dashbase
-       done
-    else
-       echo "no presto secrets is found in dashbase namespace"
-    fi
-
+    sleep 10
+    # delete pvc
+    delete_dashbase_pvc
+    # delete secrets
+    delete_dashbase_secrets
     # delete storageclass
     remove_storageclass
     #  delete helm tiller
@@ -129,25 +248,23 @@ if [ "$(kubectl get namespace |grep -c dashbase)" -eq "1" ]; then
     # delete clusterrolebindings
     remove_clusterrolebindings
     # delete sa dashadmin account
-    if [ "$(kubectl get sa -n dashbase |grep -c dashadmin)" -gt "0" ]; then
-       echo "service account dashadmin exists"
-       echo "delete service account dashadmin"
-       kubectl delete sa dashadmin -n dashbase
-    else
-       echo "service account dashadmin is not found in dashbase namespace"
-    fi
+    remove_sa_dashadmin
     # delete sa tiller account
     remove_sa_tiller
     # delete namespace
     kubectl delete namespace dashbase
   fi
-
 else
-  echo "dashbase namespace is not found"
+  log_info "No dashbase namespace is found in this K8s cluster"
   remove_storageclass
   remove_tiller
   remove_clusterrolebindings
   remove_sa_tiller
 fi
+
+
+
+
+
 
 
