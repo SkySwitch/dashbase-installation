c94256e4890bb9b17d7b78e35cf9e67ba5ac253e
rluidash <56131833+rluidash@users.noreply.github.com>
2020-08-26 13:57:28 -0700

    DB-2541 update dashbase installer support 2.3.0 release (#231)
    * updated dashbase version to 2.3.1-rc5
    
    * updated admindash-server-sts_helm3.yaml
    
    * update installer dashbase version and 2.3.0 call flow  feature
    
    * updated admin pod image to nightly-93c2fb
    
    * update installer script for callflow netsapien log option

--- deployment-tools/check-K8s-nodes-resources-usage.sh
+++ deployment-tools/check-K8s-nodes-resources-usage.sh
@@ -3,6 +3,9 @@
 NODES=$(kubectl get nodes |sed -e 1d |awk '{print $1}' |tr '\n' ' ')
 
 for ND in $NODES ; do
-   echo $ND
-   kubectl describe node $ND  |grep -E "cpu|memory" |grep -ivE "cpu\:|memory\:" |grep -iv MemoryPressure
+   echo -e "$ND\t \
+cpu $(kubectl describe node $ND |grep cpu |tail -1 |tail -1 |awk '{print $3,$5}')\t \
+memory $(kubectl describe node $ND |grep memory |tail -1 |tail -1 |awk '{print $3,$5}')\t \
+instance-id $(kubectl describe node $ND |grep "ProviderID:" |awk '{ print $2}' |cut -d'/' -f5)"
+
 done
